9be6692c6d5ecb8a9a45cb40e3e7198c643cbc6c
181268 <lujieni1017>
2020-09-10 14:35:11 +0800

    代码提交

--- src/atomic/Test.java
+++ src/atomic/Test.java
@@ -12,19 +12,19 @@ public class Test {
         AtomicInteger a = new AtomicInteger(1);
 
         new Thread(()->{
-            int result = a.getAndAdd(1);
+             i = i + 1;//2
             try {
-                Thread.sleep(2_000);
+                Thread.sleep(2_000);//睡眠2秒的时候,别的线程已经将i的值改变了
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(result);
+            System.out.println(i);//3 因为它会读取main memory中的最新的值
         }).start();
 
         Thread.sleep(500);
 
         new Thread(()->{
-            a.getAndAdd(1);
+            i = i + 1;
         }).start();
     }
 }
